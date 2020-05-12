4107b846e827b8e039dbd877db78250519b7e20f
Penchal <vemireddy.penchalreddy>
2020-05-12 18:22:35 +0530

    Initial Commit

---
+++ StaticTableViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaticTableViewCells
+//
+//  Created by VijayNew on 12/05/20.
+//  Copyright © 2020 VijayNew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
